a5981f38441a078c10d7162a0a6a70f8abb9e87c
Alepha988 <114670131+Alepha988@users.noreply.github.com>
2022-09-29 21:49:50 +0300

    Update pr2.py

--- pr2.py
+++ pr2.py
@@ -3,5 +3,5 @@ import math
 x = float (input("Введите x "))
 y = float (input("Введите y "))
 z = float (input("Введите z "))
-s = ()
-print("s= {0:.6f}".format(s))
+s = ((5*math.atan(x)-0.25*math.acos(x)*((x+3*abs(x-y) + x*x)))/((abs(x-y) *z + x*x)))
+print("s= {0:.4}".format(s))
