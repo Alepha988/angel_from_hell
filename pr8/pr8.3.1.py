ea69af6e5b2c4c7734b08c39414f99386d20a3ee
Alepha988 <114670131+Alepha988@users.noreply.github.com>
2022-10-28 18:15:11 +0300

    Create pr8.3.1.py

---
+++ pr8/pr8.3.1.py
@@ -0,0 +1,16 @@
+a = []
+n = int(input())
+for i in range(n):
+    temp = input().split()
+    for j in range(len(temp)):
+        temp[j] = int(temp[j])
+    a.append(temp)
+flag = 1
+for i in range(n - 1):
+    for j in range(i + 1, n):
+        if a[i][j] != a[j][i]:
+            flag = 0
+if flag == 1:
+    print('Ok')
+else:
+    print('Not Ok')
