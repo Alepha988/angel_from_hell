44311251c65b690a5e8f73a0bb9e55968e2290a3
Alepha988 <114670131+Alepha988@users.noreply.github.com>
2022-11-19 16:33:53 +0300

    Update pr9.1.1.py

--- pr9/pr9.1.1.py
+++ pr9/pr9.1.1.py
@@ -1,4 +1,4 @@
-f = open('C:\\Users\\Alepha89\\PycharmProjects\\Yp\\Aleksey_Morgunov_vvod')
+f = open('C:\\Users\\Alepha98\\PycharmProjects\\Yp\\Aleksey_Morgunov_vvod')
 a = [line.replace("\n", "").split() for line in f]
 k = 0
 s = 0
@@ -8,7 +8,7 @@ for i in range(N):
 			s = s + a[i][j]
 		if a[i][j] > 0:
 			k += 1
-with open('C:\\Users\\Alepha89\\PycharmProjects\\Yp\\Aleksey_Morgunov_vivod','w') as f:
+with open('C:\\Users\\Alepha98\\PycharmProjects\\Yp\\Aleksey_Morgunov_vivod','w') as f:
 	f.write('Сумма элементов над главной диагональю: ')
 	f.write(s)
 	f.write('Кол-во положительных элементов')
