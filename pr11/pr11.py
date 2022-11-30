4c3de4292190540734751a66fdc56a7bf9dae7a2
Alepha988 <114670131+Alepha988@users.noreply.github.com>
2022-11-30 08:45:12 +0300

    Update pr11.py

--- pr11/pr11.py
+++ pr11/pr11.py
@@ -8,32 +8,32 @@ def click():
     r = requests.get(f'https://api.github.com/repos/{username}/{repositoriy}').json()
     with open('C:\\Users\\Aleph\\PycharmProjects\\pythonProject15\\pr11', 'a+') as f:
         if 'company' in r:
-            f.write(f"'company': '{r['company']}")
+            f.write(f"'company': '{r['company']}\n")
         else:
-            f.write('company: None')
+            f.write("'company: None'\n")
         if 'created_at' in r:
-            f.write(f"'created_at': '{r['created_at']}")
+            f.write(f"'created_at': '{r['created_at']}\n")
         else:
-            f.write('None')
+            f.write("'None'\n")
         if 'email' in r:
-            f.write(f"'email': '{r['email']}")
+            f.write(f"'email': '{r['email']}\n")
         else:
-            f.write('email: None')
+            f.write("'email: None'\n")
         if 'id' in r:
-            f.write(f"'id': '{r['id']}")
+            f.write(f"'id': '{r['id']}\n")
         else:
             f.write('None')
         if 'name' in r:
-            f.write(f"'name': '{r['name']}")
+            f.write(f"'name': '{r['name']}\n")
         else:
             f.write('None')
         if 'url' in r:
-            f.write(f"'url': '{r['url']}")
+            f.write(f"'url': '{r['url']}\n")
         else:
             f.write('None')
 
 window = Tk()
-window.title('Morgunov Aleksey')
+window.title('Aleksey Morgunov')
 window.geometry('600x300')
 lbl = Label(window, text='Введите репозиторий', font=('Times New Roman', 20))
 lbl.grid(column=0, row=0)
